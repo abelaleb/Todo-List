ce02148e64903b1828017cd92e493d80c098d8fb
abel <abelalebachewasfaw.gmail.com>
2024-08-23 00:45:34 +0300

    Fix issues on the plugin and style

--- webpack.common.js
+++ webpack.common.js
@@ -1,16 +1,25 @@
-const path = require('path');
+const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const config = {
-    entry: "./src/index.js",
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "main.js",
-        clean: true,
-
-    },
-    plugins: [new HtmlWebpackPlugin({
-        template: "./src/index.html"
-    })]
-}
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/, // Test for .css files
+        use: ["style-loader", "css-loader"], // Use these loaders for CSS files
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+};
 module.exports = config;

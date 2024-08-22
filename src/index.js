95838db9762c1fac40557921485a3015dbe856a9
abel <abelalebachewasfaw.gmail.com>
2024-08-23 00:47:43 +0300

    Add functionality to the webpage

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-alert("well");
+import "./style.css";
+import { blankProjectLoad } from "./project.js";
+import { blankTodosListLoad } from "./todo-list.js";
+import { format, addDays } from "date-fns";
+
+blankProjectLoad();
+blankTodosListLoad();
+
 const container = document.querySelector(".container");
-container.textContent = "hi";
+const dialog = document.getElementById("TodosDialog");
+const addTodosBtn = document.getElementById("addTodosBtn");
+const closeDialogBtn = document.getElementById("closeDialogBtn");
+
+addTodosBtn.addEventListener("click", () => {
+  dialog.showModal();
+});
+closeDialogBtn.addEventListener("click", () => {
+  dialog.close();
+});
+
+localStorage.setItem("colorSetting ", "wow");
